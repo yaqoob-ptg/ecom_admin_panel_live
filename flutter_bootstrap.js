52e82460e541d8ddf9f249fd7f3a726c2e491d79
yaqoob-ptg <yaqoob.ptg@gmail.com>
2026-04-28 18:41:11 +0500

    fixing loading problem

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2644963501" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1218245388" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
